5151af22739296ccc196fb7952dd5d8a598ad4fe
Kaylee Horner <kayleeahorner@gmail.com>
2021-01-30 01:46:15 -0600

    added load file call and removed form style

--- employee-directory/src/utils/API.js
+++ employee-directory/src/utils/API.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const BASEURL = "https://randomuser.me/api/?results=500&nat=us";
+const BASEURL = "https://randomuser.me/api/?results=100&format=json&inc=name,email,phone,picture,id&nat=us&callback=randomuserdata";
 
 
 export default {
-  search: function() {
+  loadEmployees: function() {
     return axios.get(BASEURL);
   }
 };
