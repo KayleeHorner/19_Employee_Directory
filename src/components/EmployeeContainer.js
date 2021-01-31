ccb1a119fd8d32158d3b722c70400ee0db8bf73d
Kaylee Horner <kayleeahorner@gmail.com>
2021-01-31 16:37:59 -0600

    figured out how to get some data to load in

--- src/components/EmployeeContainer.js
+++ src/components/EmployeeContainer.js
@@ -6,71 +6,83 @@ import API from "../utils/API";
 
 class EmployeeContainer extends Component {
   state = {
-    filter: "",
+    // filter: "",
     employees: [],
+    filteredEmp: [],
+    nameSort: 0,
     search: "",
   };
 
   componentDidMount() {
-   this.loadEmployees();
+    this.loadEmployees();
   }
 
-  loadEmployees = () => {
-    API.loadEmployees()
-    .then((res) => {
-      console.log(res);
-      this.setState({ employees: res.data.results });
-    })
-    .then(() => {
-      this.setState({ filter: this.state.results });
-    })
-    .catch((err) => console.log(err));
-  }
+  loadEmployees = async () => {
+    const randomEmp = await API.getEmp();
+    randomEmp.data.results.forEach((employee) => {
+      const fullName =
+        employee.name.first[0].toUpperCase() +
+        employee.name.first.substring(1) +
+        " " +
+        employee.name.last[0] +
+        employee.name.last.substring(0);
+
+      employee.name = fullName;
+    });
+    this.setState({ employees: randomEmp.data.results });
+    this.setState({ filteredEmp: randomEmp.data.results });
+  };
 
   handleInputChange = (event) => {
     if (event.target.name === "search") {
       const inputs = event.target.value;
-      this.setState(
-        {
-          search: inputs,
-        }
-      );
+      this.setState({
+        search: inputs,
+      });
     }
   };
 
-  render() {
+  // sorting = (event) => {
+  //   const name = event.currentTarget.getAttribute('name');
+  //   let sort = this.state[name + 'Sort'];
+  //   sort === 0 ? (sort = 1) : sort === 1 ? (sort = -1) : (sort = 0);
+  //   let sorted;
+  //   sorted = this.state.filtereedEmp.sort((emp1, emp2) => {
+  //     if (emp1[name] < emp2[name]) {
+  //       return sort * -1;
+  //     }
+  //     if (emp1[name] > emp2[name]) {
+  //       return sort * 1;
+  //     }
+  //     return 0;
+  //   })
+  // }
 
+  render() {
     return (
-      <React.Fragment>
       <Container>
         <div className="row">
-              <Search
-                search={this.state.search}
-                handleInputChange={this.handleInputChange}
-              />
+          <Search
+            search={this.state.search}
+            handleInputChange={this.handleInputChange}
+          />
         </div>
 
         <div className="row">
-    
-        
-   
-       
+          {this.state.employees.map((emp) => {
+            return (
               <UserDetail
-                  firstname={this.state.results.name.first}
-                  lastname={this.state.resuls.name.last}
-                  email={this.state.results.email}
-                  location={this.state.results.location}
-                  phone={this.state.results.phone}
-                /> 
-           
-      
+                firstname={emp.name}
+                lastname={emp.name.last}
+                email={emp.email}
+                picture={emp.picture.thumbnail}
+                phone={emp.phone}
+              />
+            );
+          })}
         </div>
       </Container>
-      </React.Fragment>
     );
-
-
-
   }
 }
 
