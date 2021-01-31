3ca26b4987f04e6f17f0bf9e3cc6e8712aa24545
Kaylee Horner <kayleeahorner@gmail.com>
2021-01-30 22:20:43 -0600

    figured out how to get api data into console

--- employee-directory/src/components/EmployeeContainer.js
+++ employee-directory/src/components/EmployeeContainer.js
@@ -1,109 +1,95 @@
 import React, { Component } from "react";
 import Container from "./Container";
-// import Col from "./Col";
-import Div from "./Div";
-import Span from "./Header/SpanHead";
 import Search from "./Search/Search";
 import UserDetail from "./UserDetail";
 import API from "../utils/API";
 
-
 class EmployeeContainer extends Component {
+  state = {
+    filter: "",
+    employees: [],
+    search: "",
+  };
 
-    state = {
-      employees: [],
-        results: [],
-        search: '',
-    };
-  
-    componentDidMount() {
-        this.loadEmployees();
-        // this.searchUsers();
-      }
-  
+  componentDidMount() {
+   this.loadEmployees();
+  }
 
-      searchUsers = () => {
-        API.search()
-          .then(res => this.setState({ results: res.data.results }
-            // , () => this.search()
-            ))
-          .catch(err => console.log(err));
-      };
+  loadEmployees = () => {
+    API.loadEmployees()
+    .then((res) => {
+      console.log(res);
+      this.setState({ employees: res.data.results });
+    })
+    .then(() => {
+      this.setState({ filter: this.state.results });
+    })
+    .catch((err) => console.log(err));
+  }
 
-      loadEmployees = () => {
-        API.load()
-          .then(res => this.setState({ results: res.data.results }
-            // , () => this.search()
-            ))
-          .catch(err => console.log(err));
-      };
-    
-      handleInputChange = event => {
-        const value = event.target.value;
-        const name = event.target.name;
-        this.setState({
-          [name]: value
-        }, 
-        () => {
-            this.search();
-        // });
-      });
+  handleInputChange = (event) => {
+    if (event.target.name === "search") {
+      const inputs = event.target.value;
+      this.setState(
+        {
+          search: inputs,
         }
-    
-      
-      handleFormSubmit = event => {
-        event.preventDefault();
-        this.searchUsers(this.state.search);
-      };
-    
-      search = () => {
-       const filteredResults = this.state.results.filter((result) => {
-         result.email.includes(this.state.search)
-       });
-       this.setState({ filteredResults: filteredResults });
-      };
-
-      render() {
-        return (
-          <Container>
-             <Span />
-              <div className="row">
-              <div className="col">
-                  <div>
-                      <Search>
-
-                    <input
-                    search={this.state.search}
-                    handleInputChange={this.handleInputChange}
-                    handleFormSubmit={this.handleFormSubmit}
-                  />
-                  </Search>
-         </div>
-              </div>
-              </div>
-            <div className="row">
-              <div className="col">
-                <Div>
-                  
-                  {this.state.name ? (
-                    <UserDetail
-                    image={this.state.results.picture.thumbnail}
-                      firstname={this.state.results.name.first}
-                      lastname={this.state.results.name.last}
-                      email={this.state.results.email}
-                      location={this.state.results.location}
-                      phone={this.state.phone}
-                    />
-                  ) : (
-                    <h3>No Results to Display</h3>
-                  )}
-                </Div>
-              </div>
-             
-            </div>
-          </Container>
-        );
-      }
+      );
     }
+  };
+
+  render() {
+
+    return (
+      <Container>
+        <div className="row">
+              <Search
+                search={this.state.search}
+                handleInputChange={this.handleInputChange}
+              />
+        </div>
+
+        <div className="row">
+        <table className="container">
+        <thead>
+          <tr>
+              <th>ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr> 
+            <td>{this.state.employees.map.id}</td>
+            <td>{this.state.item.name.first}</td>
+            <td>{this.state.item.name.last}</td>
+            <td>{this.state.item.email}</td>
+            <td>{this.state.item.phone}</td>
+          </tr>
+        </tbody>
+
+      </table>
+   
+            {/* <div> */}
+              {/* <UserDetail
+                  image={this.state.results.picture.thumbnail}
+                  firstname={this.state.results.name.first}
+                  lastname={this.state.results.name.last}
+                  email={this.state.results.email}
+                  location={this.state.results.location}
+                  phone={this.state.results.phone}
+                /> */}
+           
+          {/* </div> */}
+        </div>
+      </Container>
+    );
+
+
+
+  }
+}
 
 export default EmployeeContainer;
