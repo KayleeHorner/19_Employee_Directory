8c969f6ebf391faa6017447a0d8d619720eec070
Kaylee Horner <kayleeahorner@gmail.com>
2021-01-30 22:21:10 -0600

    attempting different methods to pull info in

--- employee-directory/src/components/UserDetail.js
+++ employee-directory/src/components/UserDetail.js
@@ -1,37 +1,57 @@
 import React from "react";
 
 const UserDetail = (props) => {
+  <UserDetail
+  firstname={props.name.first}
+  lastname={props.name.last}
+  email={props.email}
+  location={props.location}
+  phone={props.phone}
+/> 
 
-  return (
-    <div className="row">
-
-  <table className="container">
-        <thead>
-          <tr>
-              <th>Picture</th>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Phone</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <td><img src={props.image} alt={props.name} /></td>
-            <td>{props.id}</td>
-            <td>{props.name}</td>
-            <td>{props.email}</td>
-            <td>{props.phone}</td>
-          </tr>
-        </tbody>
-      </table>
-
-
-
-
-    </div>
-  );
 }
+
+
+{this.state.employees.map((employees) => (
+  <Table  
+   first={employees.first}
+   last={employees.last}
+   email={employees.email}
+   brand={employees.brand}
+  />
+
+//   return (
+//     <div className="row">
+
+//   <table className="container">
+//         <thead>
+//           <tr>
+//               <th>Picture</th>
+//               <th>ID</th>
+//               <th>First Name</th>
+//               <th>Last Name</th>
+//               <th>Email</th>
+//               <th>Phone</th>
+//           </tr>
+//         </thead>
+
+//         <tbody>
+//           <tr>
+//             <td><img src={props.image} alt={props.name} /></td>
+//             <td>{props.id}</td>
+//             <td>{props.name}</td>
+//             <td>{props.name}</td>
+//             <td>{props.email}</td>
+//             <td>{props.phone}</td>
+//           </tr>
+//         </tbody>
+//       </table>
+
+
+
+
+//     </div>
+//   );
+// }
 
 export default UserDetail;
